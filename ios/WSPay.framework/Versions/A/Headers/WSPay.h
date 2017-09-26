25659d638ba9387f902e4db3d130396f9a20e1cd
houli <houli@wesai.com>
2017-09-26 10:26:18 +0800

    add 替换 SDK json

--- ios/WSPay.framework/Versions/A/Headers/WSPay.h
+++ ios/WSPay.framework/Versions/A/Headers/WSPay.h
@@ -113,4 +113,14 @@ typedef void(^WSPCommonBlock)(void); // 通用的回调SDK
 
 -(void)setHostUrl:(NSString *)url;
 
+/*
+ 获取ip
+ ClientID：标准UUID形式的客户端唯一标识。
+AppDeviceID：App端的设备ID。如果获取不到，由客户端生成。（格式为：15位以上的随机字符串）
+ userAge:（用户游戏年龄)代表用户注册之时至此时此刻的相对时间，单位为秒钟。
+ */
++ (void)getAvailableIPWithAppDeviceID:(NSString *)appDeviceID ClientID:(NSString *)clientID userAge:(long)userAge ipCount:(int)ipCount completion:(void(^)(id data))completion;
+
++ (void)setIPHostUrl:(NSString *)url;
+
 @end
